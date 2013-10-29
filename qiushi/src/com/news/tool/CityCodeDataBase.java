8e1417da6e6abf0429bc74a1280477fc5a0b524a
luolb <137045198@qq.com>
2013-10-29 19:57:08 +0800

    init database dir

--- qiushi/src/com/news/tool/CityCodeDataBase.java
+++ qiushi/src/com/news/tool/CityCodeDataBase.java
@@ -1,6 +1,7 @@
 package com.news.tool;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,23 +18,49 @@ public class CityCodeDataBase {
 	private static final CityCodeDataBase m_instance = new CityCodeDataBase();
 	private Context m_context;
 	
+	public static final String databaseDir = "/sdcard/meinvqiushi/db/";
 	
 	public static CityCodeDataBase getInstance()
 	{
 		return m_instance;
 	}
 	
-	public void initDataBase(Context context)
+	public boolean initDataBase(Context context)
 	{
 		Log.i("cityCode", "InitDataBase2");
-		this.m_context = context;
-		int i = 0;
-		int j = 0;
-		String fileName = null;
-		for(i=1,j=5;i<=3;++i,j+=2){
-			fileName = "code" + i + ".txt";
-			loadCodeFile(fileName,j);
+		boolean ret = false;
+		
+		if((ret = initDir()) ){
+			this.m_context = context;
+			int i = 0;
+			int j = 0;
+			String fileName = null;
+			for(i=1,j=5;i<=3;++i,j+=2){
+				fileName = "code" + i + ".txt";
+				loadCodeFile(fileName,j);
+			}
+		}else{
+			ret =  false;
 		}
+		
+		return ret;
+	}
+	
+	protected boolean initDir()
+	{
+		boolean result = false;
+		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {   
+    		// sd card ¿ÉÓÃ                          
+    		 File dirFile = new File(CityCodeDataBase.databaseDir);  
+    	     if(!dirFile.exists()){        	
+    	          result=dirFile.mkdirs();
+    	     }
+    	     
+		}else{
+			result = false;
+		}
+		
+		return result;
 	}
 	
 	protected void loadCodeFile(String fielName,int j)
@@ -41,7 +68,7 @@ public class CityCodeDataBase {
 		AssetManager asset_manager=this.m_context.getAssets();
 		InputStream input_stream;
 		List<String> level=new ArrayList<String>();
-		String path=Environment.getExternalStorageDirectory()+"/LocateCity/"+DatabaseUtil.database_name;
+		String path = CityCodeDataBase.databaseDir+DatabaseUtil.database_name;
 		
 		try {
 			
