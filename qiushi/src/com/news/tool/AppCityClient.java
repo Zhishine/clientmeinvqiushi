6f9e8277ae9c831dfebb9ca6b27d9da4f02b0e18
luolb <137045198@qq.com>
2013-10-31 15:40:42 +0800

    cityClient

--- qiushi/src/com/news/tool/AppCityClient.java
+++ qiushi/src/com/news/tool/AppCityClient.java
@@ -1,6 +1,9 @@
 package com.news.tool;
 
-import android.content.Context;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -8,31 +11,20 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.news.modal.MAddressComponet;
 import com.news.modal.MCity;
-import com.news.modal.MTitude;
-import com.news.modal.MWeatherInfo;
+
 
 public class AppCityClient {
 	
-	
 	private  AsyncHttpClient m_client = new AsyncHttpClient();
-	//private Context m_context = null;
-	private AppDataObserver m_observer = null;
-	private MAddressComponet m_addressCom = new MAddressComponet();
-	
-	public AppCityClient(AppDataObserver observer)
-	{
-		this.m_observer = observer;
-	}
+	private MAddressComponet m_address_detail = new MAddressComponet();
 	
 	public MAddressComponet getCityInfo()
 	{
-		TitudeDataClient titude = TitudeDataClient.getInstance();
-		titude.Init((Context)this.m_observer);
-		
-		MTitude mtitude = titude.getLongitudeAndLatitude();
+		/*location */
 		//http://api.map.baidu.com/geocoder/v2/?ak=129c55029c32cd9ea2d15f0fb1d4cb2f&location=39.983424,116.322987&output=json
-		String url = "http://api.map.baidu.com/geocoder/v2/?ak=129c55029c32cd9ea2d15f0fb1d4cb2f&location=" + 
-				mtitude.latitude + "," + mtitude.longitude + "&output=json";
+		
+		/*ip*/
+		String url = "http://api.map.baidu.com/location/ip?ak=129c55029c32cd9ea2d15f0fb1d4cb2f";
 		
 		m_client.get(url, new AsyncHttpResponseHandler() {
 	           @Override
@@ -40,14 +32,22 @@ public class AppCityClient {
 	              Gson gson = new Gson();
 	              //MWeatherInfo appData=gson.fromJson(response,new TypeToken<MWeatherInfo>(){}.getType());
 	              //m_observer.getAppWeatherResponse(appData);
-	              
+	              URLDecoder ud = new URLDecoder();
 	              MCity appData = gson.fromJson(response,new TypeToken<MCity>(){}.getType());
-	              m_addressCom = appData.addressComponent;
-	              //m_observer.getAppCityResponse(appData.addressComponent);
+	              try {
+	            	  	m_address_detail.city = ud.decode(appData.content.address_detail.city,"utf-8");
+	            	  	m_address_detail.district = ud.decode(appData.content.address_detail.district,"utf-8");
+	            	  	m_address_detail.province = ud.decode(appData.content.address_detail.province,"utf-8");
+	            	  	m_address_detail.street = ud.decode(appData.content.address_detail.street,"utf-8");
+					
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	          }
 	    });
 		
-		return m_addressCom;
+		return m_address_detail;
 	}
 	
 }
